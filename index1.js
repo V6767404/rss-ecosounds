10658095adfa9f41a2b86e08ab04bde3616e846b
Victor <victor@victor.by>
2022-02-06 00:35:29 +0300

    fix: add cache image

--- index1.js
+++ index1.js
@@ -1,34 +1,34 @@
 let isPlay = false;
 const button = document.querySelector("button");
 const logo = document.querySelector("logo");
+const spec = ["drozd", "forest", "javoronok", "slavka", "solovey", "zarynka"];
 
 const audio = new Audio();
 audio.src = "./assets/audio/forest.mp3";
 
 function playAudio() {
-    audio.currentTime = 0;
-    audio.play();
-    button.classList.add("pause");
+  audio.currentTime = 0;
+  audio.play();
+  button.classList.add("pause");
 }
 
 function pauseAudio() {
-    audio.pause();
-    button.classList.remove("pause");
+  audio.pause();
+  button.classList.remove("pause");
 }
 
 function chooseBtn() {
-    if (isPlay) {
-        isPlay = false;
-        pauseAudio();
-    } else {
-        isPlay = true;
-        playAudio();
-    }
+  if (isPlay) {
+    isPlay = false;
+    pauseAudio();
+  } else {
+    isPlay = true;
+    playAudio();
+  }
 }
 
-//  MAIN BTN 
+/////////  MAIN BTN /////////
 button.addEventListener("click", chooseBtn);
-
 
 const navLinks = document.querySelectorAll(".nav-item");
 const main = document.querySelector("main");
@@ -36,53 +36,71 @@ const dataItem = document.querySelector("data-item");
 
 // console.log(navLinks[1].dataset.item);
 
-// NAV MENU 
-const changeMenu = (element) => {
+///////// NAV MENU /////////
 
-    switch (element.dataset.item) {
-        case ("drozd"):
-            // console.log("drozd");
-            audio.src = "./assets/audio/drozd.mp3";
-            // main.classList.add("drozd");
-            // main.style.backgroundImage="url(\"./assets/img/slavka.jpg\")";
-            main.className = "main drozd";
-            // main.setAttribute("class", "main drozd");
-            playAudio();
-            isPlay = true;
-            break;
-        case ("javoronok"):
-            console.log("javoronok");
-            audio.src = "./assets/audio/javoronok.mp3";
-            main.className = "main javoronok";
-            playAudio();
-            isPlay = true;
-            break;
-        case ("slavka"):
-            audio.src = "./assets/audio/slavka.mp3";
-            main.className = "main slavka";
-            playAudio();
-            isPlay = true;
-            break;
-        case ("solovey"):
-            audio.src = "./assets/audio/solovey.mp3";
-            main.className = "main solovey";
-            playAudio();
-            isPlay = true;
-            break;
-        case ("zarynka"):
-            audio.src = "./assets/audio/zarynka.mp3";
-            main.className = "main zarynka";
-            playAudio();
-            isPlay = true;
-            break;
-        case ("forest"):
-            // console.log("zarynka");
-            audio.src = "./assets/audio/forest.mp3";
-            main.className = "main";
-            playAudio();
-            isPlay = true;
-            break;
-    }
+// console.log(spec[0]);
+// console.log('url("./assets/img/' + spec[0] + '.jpg")');
+// console.log(`url("./assets/img/${spec[0]}.jpg")`);
+
+const changeMenu = (element) => {
+  switch (element.dataset.item) {
+    case "drozd":
+      // console.log("drozd");
+      audio.src = "./assets/audio/drozd.mp3";
+      main.className = "main drozd";
+      // main.setAttribute("class", "main drozd");
+      //   main.style.backgroundImage = 'url("./assets/img/drozd.jpg")';
+      //   main.style.backgroundImage = 'url("./assets/img/' + spec[0] + '.jpg")';
+      //   main.style.backgroundImage = `url("./assets/img/${spec[0]}.jpg")`;
+      playAudio();
+      isPlay = true;
+      break;
+    case "javoronok":
+      console.log("javoronok");
+      audio.src = "./assets/audio/javoronok.mp3";
+      main.className = "main javoronok";
+      playAudio();
+      isPlay = true;
+      break;
+    case "slavka":
+      audio.src = "./assets/audio/slavka.mp3";
+      main.className = "main slavka";
+      playAudio();
+      isPlay = true;
+      break;
+    case "solovey":
+      audio.src = "./assets/audio/solovey.mp3";
+      main.className = "main solovey";
+      playAudio();
+      isPlay = true;
+      break;
+    case "zarynka":
+      audio.src = "./assets/audio/zarynka.mp3";
+      main.className = "main zarynka";
+      playAudio();
+      isPlay = true;
+      break;
+    case "forest":
+      audio.src = "./assets/audio/forest.mp3";
+      main.className = "main";
+      playAudio();
+      isPlay = true;
+      break;
+  }
 };
 
-navLinks.forEach((el) => el.addEventListener("click", function () { changeMenu(el) }));
+navLinks.forEach((el) =>
+  el.addEventListener("click", function () {
+    changeMenu(el);
+  })
+);
+
+// Кеширование изображений
+function preloadImages() {
+  for (let i = 0; i <= 5; i++) {
+    const img = new Image();
+    // img.src = "./assets/img/" + spec[i] + ".jpg";
+    img.src = `./assets/img/${spec[i]}.jpg`;
+  }
+}
+preloadImages();
--- z/z_index.js
+++ z/z_index.js
@@ -10,22 +10,73 @@
 //     (play[_0x469ae8(0x1f2)]["backgroundImage"] = "url(\x22./assets/svg/play.svg\x22)"), audio[_0x469ae8(0x1f5)](), (isPlay = ![]);
 // }
 
-
-
 // https://github.com/rolling-scopes-school/tasks/blob/master/tasks/portfolio/portfolio-part3-hints.md
 
-const portfolioBtn = document.querySelector('.portfolio-btn');
-const portfolioImages = document.querySelectorAll('.portfolio-image');
+const portfolioBtn = document.querySelector(".portfolio-btn");
+const portfolioImages = document.querySelectorAll(".portfolio-image");
 
-portfolioBtn.addEventListener('click', () => {
-    portfolioImage.src = "./assets/img/winter/1.jpg"
- });
+portfolioBtn.addEventListener("click", () => {
+  portfolioImage.src = "./assets/img/winter/1.jpg";
+});
 
-portfolioImages.forEach((img, index) => img.src = `./assets/img/winter/${index + 1}.jpg`);
+portfolioImages.forEach(
+  (img, index) => (img.src = `./assets/img/winter/${index + 1}.jpg`)
+);
 // img.src = "./assets/img/winter/" + (index + 1) + ".jpg"
 
 function changeImage(event) {
-    if(event.target.classList.contains('portfolio-btn')) {
-      // здесь код функции, меняющей src изображений
-    }
+  if (event.target.classList.contains("portfolio-btn")) {
+    // здесь код функции, меняющей src изображений
   }
+}
+
+// Советы по выполнению задания Portfolio#3-Добавление функционала
+// https://github.com/rolling-scopes-school/tasks/blob/master/tasks/portfolio/portfolio-part3-hints.md
+const portfolioBtn = document.querySelector(".portfolio-btn");
+const portfolioImage = document.querySelector(".portfolio-image");
+
+portfolioBtn.addEventListener("click", () => {
+  portfolioImage.src = "./assets/img/winter/1.jpg";
+});
+
+portfolioImages.forEach(
+  (img, index) => (img.src = `./assets/img/winter/${index + 1}.jpg`)
+);
+
+function changeImage(event) {
+  if (event.target.classList.contains("portfolio-btn")) {
+    // здесь код функции, меняющей src изображений
+  }
+}
+
+// data-атрибуты
+<div class="portfolio-btns">
+  <button class="portfolio-btn" data-season="winter">
+    Winter
+  </button>
+  <button class="portfolio-btn" data-season="spring">
+    Spring
+  </button>
+  <button class="portfolio-btn" data-season="summer">
+    Summer
+  </button>
+  <button class="portfolio-btn" data-season="autumn">
+    Autumn
+  </button>
+</div>;
+// Кеширование изображений
+function preloadSummerImages() {
+  for (let i = 1; i <= 6; i++) {
+    const img = new Image();
+    img.src = `./assets/img/summer/${i}.jpg`;
+  }
+}
+preloadSummerImages();
+
+const seasons = ["winter", "spring", "summer", "autumn"];
+// Подсветка активной кнопки
+const portfolioBtn = document.querySelector(".portfolio-btn");
+
+portfolioBtn.style.backgroundColor = "#bdae82";
+portfolioBtn.style.color = "#000";
+portfolioBtn.classList.add("active");
